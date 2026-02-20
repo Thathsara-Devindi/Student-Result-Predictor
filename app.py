d455d0803c74177978d3d360f0b21140b09fe8de
Thathsara-Devindi <166741715+Thathsara-Devindi@users.noreply.github.com>
2026-02-20 12:14:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,15 +24,9 @@ if st.button("Predict Score"):
     # Data tika AI ekata galapena format ekata hadamu
     input_data = np.array([[hours, attendance, participation]])
     
-    # app.py file eke prediction line ekata yata mehema danna
+    # Prediction eka gannawa
     prediction = model.predict(input_data)
     final_score = prediction[0][0]
-
-# Score eka 0 saha 100 athara vitarak thiyaganna meka danna
-    final_score = np.clip(final_score, 0, 100) 
-
-# Result eka pennanawa
-    st.success(f"The predicted Total Score is: {final_score:.2f}%")
     
     # Result eka pennanawa
     st.success(f"The predicted Total Score is: {final_score:.2f}%")
